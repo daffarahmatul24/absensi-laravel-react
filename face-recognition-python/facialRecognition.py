38f66adf86b855bc982302ee1679367181538dca
Daffa Rahmatul <davarahmatul05@gmail.com>
2026-02-27 00:03:57 +0700

    progress: Save user face when attendance

--- face-recognition-python/facialRecognition.py
+++ face-recognition-python/facialRecognition.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import os
+import time
 from database import fetchUser, storeAttendance
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -8,6 +9,10 @@ recognition = True
 ask = False
 usermame = ''
 userId= ''
+notMe= []
+saving = False
+saved = False
+start = 0
 
 recorgnizer = cv2.face.LBPHFaceRecognizer_create()
 recorgnizer.read('trainer/trainer.yml')
@@ -40,28 +45,45 @@ while True:
                 
                 cv2.putText(frame, str(username) + str(userId), (x+5,y+5), font, 1, (255,255,255))
                 
-                recognition = False
-                ask = True
+                if id not in notMe:
+                    recognition = False
+                    ask = True
             else:
                 cv2.putText(frame, str('unknwon'), (x+5,y+5), font, 1, (255,255,255))
+    if(saving):   
+        cv2.putText(frame, username + ' Terima Kasih telah hadir', (30,450), font, 1, (0,255,0))
         
-    if(ask):
+        if(saved):
+            storeAttendance(userId)
+            cv2.imwrite('images/' + username + "-" + str(int(time.time())) + '.jpg', frame)     
+            saved = False
+            
+        #reset setelah timer 2 detik    
+        if(int(time.time()) > start):
+            notMe = []
+            recognition = True
+            ask = False
+            saving = False
+    elif(ask):
         cv2.putText(frame, "Apakah Kamu", (30, 350), font, 1, (255,255,255))
         cv2.putText(frame, username, (30, 385), font, 1, (255,255,255))
         cv2.putText(frame, "Tidak (x)", (30, 430), font, 1, (255,255,255))
         cv2.putText(frame, "Ya (enter)", (180, 430), font, 1, (255,255,255))
         
-            
+                    
     cv2.imshow('Face Recognition', frame)
 
     k = cv2.waitKey(10)
     if(k == 27):
         break
     elif(k == 120): #"X"
+        notMe.append(userId)
         recognition = True
         ask = False
     elif(k == 13):
-        storeAttendance(userId)
+        saved = True
+        saving = True
+        start = int(time.time()) + 2 
 
 print("[INFO] EXIT")
 cam.release()
